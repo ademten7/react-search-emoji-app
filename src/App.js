8e021ceebc5b408b6e4e05f9411d9977b150055b
ademten82 <ademten7@gmail.com>
2021-11-03 11:51:08 +0100

    created emoji app

--- src/App.js
+++ src/App.js
@@ -37,3 +37,18 @@ function App() {
 }
 
 export default App;
+
+//deployment steps
+// 1: add homepage property in package json
+//"homepage":"."
+//2. install gh-pages(npm i gh-pages)
+//3. add 2 more scripts into your package json file
+/* "scripts":{
+      ....
+      "predeploy": "npm run build",
+      "deploy":"gh-pages -d build"
+    }
+    */
+//4. make sure that you have remote origin( repo is connected to github)
+
+//5. deploy your project : npm run deploy
